465e532b56a7163b3fde6e07b7942b9dd76b327a
Jonas Pedersen <jonas@pedersen.ninja>
2019-01-02 19:10:47 +0100

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+import pydanfossair
+from setuptools import setup
+
+
+setup(name='pydanfossair',
+      version=pydanfossair.__version__,
+      description='What the module does',
+      url='https://github.com/JonasPed/pydanfoss-air',
+      author='Jonas Pedersen',
+      author_email='jonas@pedersen.ninja',
+      license='Apache 2',
+      packages=['pydanfossair'])
+
