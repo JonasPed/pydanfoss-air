99e8fe1989a729cca58f1bdd5917466ceb4d341d
mschlappa <mschlappa@gmx.de>
2024-10-29 20:56:26 +0100

    fix: added missing automatic_bypass section

--- pydanfossair/danfossclient.py
+++ pydanfossair/danfossclient.py
@@ -62,6 +62,10 @@ class DanfossClient:
                            UpdateCommand.bypass_deactivate}:
                 return self._read_bit(ReadCommand.bypass, socket)
 
+            if command in {UpdateCommand.automatic_bypass_activate,
+                           UpdateCommand.automatic_bypass_deactivate}:
+                return self._read_bit(ReadCommand.bypass, socket)
+
             if command in {UpdateCommand.operation_mode_demand}:
                 return self._read_bit(ReadCommand.operation_mode, socket)
 
